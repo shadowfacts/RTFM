8d8f8e464001dc8ff3ef0bb0b292cad97aeb6a43
Shadowfacts <me@shadowfacts.net>
2016-10-16 14:39:15 -0400

    Add Underwater Utilities info

---
+++ _projects/underwater-utilities.md
@@ -0,0 +1,9 @@
+---
+layout: project
+title: "Underwater Utilities"
+date: 2016-10-16 14:03:42 -0400
+type: project
+project: "underwater-utilities"
+---
+
+[CurseForge Page](http://minecraft.curseforge.com/projects/underwater-utilities)
--- js/main.js
+++ js/main.js
@@ -8,4 +8,5 @@ MC.register("InductionCharger", "/sheets/IC.png", "/sheets/IC.json");
 MC.register("SleepingBag", "/sheets/SleepingBag.png", "/sheets/SleepingBag.json");
 MC.register("ExtraRails", "/sheets/ExtraRails.png", "/sheets/ExtraRails.json");
 MC.register("endergizer", "/sheets/endergizer.png", "/sheets/endergizer.json");
+MC.register("underwaterutilities", "/sheets/UnderwaterUtilities.png", "/sheets/UnderwaterUtilities.json");
 MC.init();
