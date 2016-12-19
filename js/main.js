c82160d1084afa9c23e601348cd7ff004f6998c5
Shadowfacts <me@shadowfacts.net>
2016-12-19 09:52:49 -0500

    Update Funnels

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ MC.smeltingBackground.src = "/MC.js/smelting-recipe.png";
 MC.register("Minecraft", "/MC.js/sheet.png", "/MC.js/sheet-min.json")
 MC.register("ShadowMC", "/sheets/ShadowMC.png", "/sheets/ShadowMC.json");
 MC.register("YeOldeTanks", "/sheets/YOT.png", "/sheets/YOT.json");
-MC.register("Funnels", "/sheets/Funnels.png", "/sheets/Funnels.json");
+MC.register("Funnels", "/sheets/funnels.png", "/sheets/funnels.json");
 MC.register("InductionCharger", "/sheets/IC.png", "/sheets/IC.json");
 MC.register("SleepingBag", "/sheets/SleepingBag.png", "/sheets/SleepingBag.json");
 MC.register("ExtraRails", "/sheets/ExtraRails.png", "/sheets/ExtraRails.json");
